eb571233728533690c9e14f482a1a29e19ce8604
Alex Popa <alexandru.popa.87@gmail.com>
2016-12-03 19:54:22 +0100

    Commit

--- js/categories.component.js
+++ js/categories.component.js
@@ -2,7 +2,7 @@
 	
 	angular.module("MenuApp")
 	.component("categories", {
-		templateUrl: "categories.template.html",
+		templateUrl: "templates/categories.template.html",
 		binding: {
 			categories: "<"
 		}
