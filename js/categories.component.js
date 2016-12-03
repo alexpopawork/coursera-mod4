f38e7391c324c2f58361ebd4e2112a1f56ea7ec3
Alex Popa <alexandru.popa.87@gmail.com>
2016-12-03 19:59:45 +0100

    Commit

--- js/categories.component.js
+++ js/categories.component.js
@@ -3,7 +3,7 @@
 	angular.module("MenuApp")
 	.component("categories", {
 		templateUrl: "templates/categories.template.html",
-		controller: "CategoriesController as ctrl",
+		controller: "CategoriesController",
 		binding: {
 			categoriesData: "<"
 		}
